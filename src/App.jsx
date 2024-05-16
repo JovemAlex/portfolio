5a228113898de8c2d59d8ac3f98493de2243830e
JovemAlex <alexsandrolima991@hotmail.com>
2024-05-15 21:01:58 -0300

    feat: Add Header Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className='w-[1216px] max-w-[1216px]'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <div className="flex items-center font-semibold text-base gap-x-64 mt-7 mb-7">
+      
+      <h2 className="text-4xl">
+        hnt.
+      </h2>
+
+      <div className="flex gap-x-36">
+        <h2>Home</h2>
+        <h2>Stacks</h2>
+        <h2>Projects</h2>
+        <h2>Contact</h2>
+      </div>
+
+
+    </div>
+  );
+}
+ 
+export default Header;
