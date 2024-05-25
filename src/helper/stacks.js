8fec0b8674832d53c1a8401749ab8b071f42592a
JovemAlex <alexsandrolima991@hotmail.com>
2024-05-24 23:36:04 -0300

    chore: add stacks mock

--- src/helper/stacks.js
+++ src/helper/stacks.js
@@ -1,68 +1,44 @@
 const stacksArr = [
   {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
+    name: 'JavaScript',
+    imgURL: 'https://i.ibb.co/KVnMF2K/Rectangle-43.png',
+    imgALT: 'javascript-logo',
   },
   {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
-  {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
-  {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
-  {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
-  {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
-  {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
+    name: 'TypeScript',
+    imgURL: 'https://i.ibb.co/zJ297S5/Rectangle-42.png',
+    imgALT: 'typescript-logo',
   },
   {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
-  {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
+    name: 'React',
+    imgURL: 'https://i.ibb.co/mRJ6pdz/Rectangle-46.png',
+    imgALT: 'React',
   },
   {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
-  {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
+    name: 'Node Js',
+    imgURL: 'https://i.ibb.co/M5fQXn4/Rectangle-41.png',
+    imgALT: 'modejs-logo',
   },
   {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
-  }, 
+    name: 'Python',
+    imgURL: 'https://i.ibb.co/HVmD6Gn/Rectangle-40.png',
+    imgALT: 'python-logo',
+  },
   {
-    name: 'Alex',
-    imgURL: 'https:aaa',
-    imgALT: 'alex -logo',
+    name: 'Photoshop',
+    imgURL: 'https://i.ibb.co/r7VhfqR/Rectangle-45.png',
+    imgALT: 'photoshop-logo',
+  },
+  {
+    name: 'Illustrator',
+    imgURL: 'https://i.ibb.co/yFyf619/Rectangle-44.png',
+    imgALT: 'illustrator-logo',
+  },
+
+  {
+    name: 'Figma',
+    imgURL: 'https://i.ibb.co/6rjZJMH/Rectangle-47.png',
+    imgALT: 'figma-logo',
   },
 ];
 
